557cf16de01e9131ccc520d674d9cccabdb688c5
DannyPhang <68151810+dannyphang@users.noreply.github.com>
2021-11-26 01:57:22 +0800

    16

--- Java/Java test file/codeforce.java
+++ Java/Java test file/codeforce.java
@@ -1,51 +1,9 @@
-package TestFile;
-
 import java.util.*;
 
 public class Mainfile {
     
     static Scanner in2 = new Scanner(System.in);
-    static int errorMessage;
-    
     public static void main(String[] args) {
-        
-        // ClearScreen.Case1();
-        // AsteriskPattern.Case1();
-        // AsteriskPattern.Case2();
-        // AsteriskPattern.Case3();
-        // AsteriskPattern.Case4();
-        // AsteriskPattern.Case5();
-        // AsteriskPattern.Case6();
-        // AsteriskPattern.Case7();
-        // GuessNumber.Case1();
-        // ClockwiseArray.Case1();
-        // PasswordValidate.Case1();
-        // PasswordValidate.Case2();
-        // PasswordValidate.Case3();
-        // ArrayFrom.Case1();
-        // ArrayFrom.Case2();
-        // NumberTest.Case1();
-        // NumberTest.Case2();
-        // NumberTest.Case3();
-        // NumberTest.Case4();
-        // NumberTest.Case5();
-        // NumberTest.Case6();
-        // NumberTest.Case7();
-        // NumberTest.Case8();
-        // StringTest.Case1();
-        // StringTest.Case2();
-        // StringTest.Case3();
-        // InsideCode.Case1();
-        // InsideCode.Case2();
-        // InsideCode.Case3();
-        // InsideCode.Case4();
-        
-        // int num = 123456;
-        // Integer[] digit = getDigits(num);
-        // // System.out.println(Math.log10(num) + 1);
-        // for(int i = 0; i < (Math.log10(num)); i++){
-        //     System.out.println(digit[i]);
-        // }
         
         int n = in2.nextInt();
         int[] l = new int[n];
